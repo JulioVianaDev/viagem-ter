aefa8a4c26af4ece5e33afe211e9750f6086d6b3
JulioVianaDev <JulioVianaDev@gmail.com>
2023-04-11 19:29:51 -0300

    formulario de cadastro incompleto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,12 @@ import {useState,useEffect} from 'react';
 
 function App() {
   const [viagens,setViagens] = useState([]);
+  const [travel,setTravel] = useState({
+    nome: '',
+    data: '',
+    desc: '',
+    price: 0
+  })
 
   useEffect(()=>{
     axios.get("http://localhost:3001/api/v1/travels")
@@ -11,8 +17,31 @@ function App() {
       .catch(erro=> console.log("deu erro no getAPi: ",erro))
   },[])
 
+  const cadastrarViagem=(travel)=>{
+    axios.post("http://localhost:3001/api/v1/travels",{travel})
+      .then(res=>{
+        console.log(res.data);
+        setViagens([...viagens,res.data])
+      })
+  }
+
+  function EnvioFormulario(e){
+    e.preventDefault()
+    cadastrarViagem(travel)
+  }
   return (
     <div>
+      <form onSubmit={EnvioFormulario}>
+        <label>Nome</label>
+        <input 
+          id="nome"
+          required
+          onChange={(e)=>setTravel({...travel,nome: e.target.value})}
+          value={travel.nome}
+          placeholder="Digite o nome da viagem"
+        />
+        <button type='submit'>Cadastrar viagem</button>
+      </form>
       {
         viagens.map(v=>
           <h1>{v.nome}</h1>
