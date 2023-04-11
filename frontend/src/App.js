f8a24e68482a9704f0ac45e4f8edbf3b9e61044d
JulioVianaDev <JulioVianaDev@gmail.com>
2023-04-11 18:41:34 -0300

    pegando os dados da api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,16 @@
+import axios from 'axios';
+import {useState,useEffect} from 'react';
+
+
 function App() {
+  const [viagens,setViagens] = useState([]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:3001/api/v1/travels")
+      .then(res=>setViagens(res.data))
+      .catch(erro=> console.log("deu erro no getAPi: ",erro))
+  },[])
+
   return (
     <div>App</div>
   )
