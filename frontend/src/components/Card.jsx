4d333730560716697eddde3400d17374973e9b0f
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-02 18:53:09 -0300

    button

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,19 +1,27 @@
 import "./Card.css"
 import {BsFillTrash2Fill} from 'react-icons/bs'
+import { FormatarData } from "../fn-helpers/Data"
 function Card(props) {
   const {id,data,desc,price} = props
+  const dataFormatada = FormatarData(data);
+
   return (
     <div className="card">
       <h1>{props.nome}</h1>
       <div>
         <p>{desc}</p>
-        <p>{data}</p>
-        <p>{price}</p>
+        <p>{dataFormatada}</p>
+        <p>R$ {price}</p>
       </div>
       <div className="lixeixa" >
-        <button onClick={()=>props.deletarViagem(id)}>
-          <BsFillTrash2Fill size={32}/>
-        </button>
+        <div className="btns" onClick={()=>props.deletarViagem(id)}>
+          <div id="trashDelete"className="icons">
+            <BsFillTrash2Fill size={32}/>
+          </div>
+          <div className="icons">
+
+          </div>
+        </div>
       </div>
     </div>
   )
